0e41b15b41dfd1c4ad8b7d7320168dcadf884798
LuisDiegoChunga <luisdiegochunga@hotmail.com>
2016-10-14 10:17:42 -0500

    Equitel version

--- CreditosULima/src/pe/edu/ulima/creditosulima/EquitelULAdapter.java
+++ CreditosULima/src/pe/edu/ulima/creditosulima/EquitelULAdapter.java
@@ -1,5 +1,20 @@
 package pe.edu.ulima.creditosulima;
 
-public class EquitelULAdapter {
+public class EquitelULAdapter implements ULAdapter {
+
+    @Override
+    public void selLibreria(String tipo) {
+        String dni = "";
+        float deuda = 0.0f;
+        
+        if(tipo.equals("Equitel")){
+            if(dni.equals("43454543")){
+                deuda = 50000.0f;
+            }else{
+                deuda = 0.0f;
+            }
+        }
+        
+    }
     
 }
--- CreditosULima/src/pe/edu/ulima/creditosulima/ULAdapter.java
+++ CreditosULima/src/pe/edu/ulima/creditosulima/ULAdapter.java
@@ -1,5 +1,5 @@
 package pe.edu.ulima.creditosulima;
 
 public interface ULAdapter {
-    
+    public void selLibreria(String tipo);
 }
