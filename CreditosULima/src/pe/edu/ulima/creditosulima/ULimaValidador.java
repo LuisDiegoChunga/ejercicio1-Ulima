dcce61bb21d4b9210a0ea3e2e841edb8b193f0ef
LuisDiegoChunga <luisdiegochunga@hotmail.com>
2016-10-14 11:34:40 -0500

    Version Singleton

--- CreditosULima/src/pe/edu/ulima/creditosulima/ULimaValidador.java
+++ CreditosULima/src/pe/edu/ulima/creditosulima/ULimaValidador.java
@@ -5,6 +5,17 @@ public class ULimaValidador {
     // Metodo que nos dice si se le realiza el prestamo a un alumno.
     // La funcionalidad de esta clase debe ser independiente de la central
     // de riesgo utilizada.
+    private static ULimaValidador singleton = null;
+    
+    public static ULimaValidador getInstance(){
+        if(singleton == null){
+            singleton = new ULimaValidador();
+        }
+        return singleton;
+    }
+    
+    private ULimaValidador(){}
+    
     public boolean esSujetoCredito(Alumno alumno){
         return false;
     }
